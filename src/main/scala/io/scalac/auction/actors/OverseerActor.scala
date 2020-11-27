8f62a138284a43f9fe43541e5160abdc52010478
Szymon Świstun <szymon.swistun@scalac.io>
2020-11-27 15:23:09 +0100

    added OverseerActor for routing between auctions

---
+++ src/main/scala/io/scalac/auction/actors/OverseerActor.scala
@@ -0,0 +1,39 @@
+package io.scalac.auction.actors
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import io.scalac.auction.protocols._
+
+object OverseerActor {
+
+  def apply(): Behavior[GeneralProtocol] = router(Map.empty)
+
+
+  private def router(auctions: Map[String, ActorRef[GeneralProtocol]]): Behavior[GeneralProtocol] = Behaviors.receive {
+    (context, message) =>
+      message match {
+        case query: AuctionQuery =>
+          query match {
+            case CreateAuction(sender, _, _, auctionName, _, _) =>
+              if (auctions.contains(auctionName)) {
+                sender ! AuctionAlreadyExists(context.self, auctionName)
+                Behaviors.same
+              }
+              else {
+                val auction = context.spawn(AuctionActor(), query.auctionName)
+                auction ! query
+                router(auctions ++ Map(auctionName -> auction))
+              }
+            case _ =>
+              auctions.get(query.auctionName) match {
+                case Some(auction) =>
+                  auction ! query
+                  Behaviors.same
+                case None =>
+                  query.sender ! AuctionNotFound(context.self, query.auctionName)
+                  Behaviors.same
+              }
+          }
+      }
+  }
+}
